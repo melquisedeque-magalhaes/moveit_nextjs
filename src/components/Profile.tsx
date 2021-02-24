312454762107bb12a6a0e543aa13ce4399a98a55
Melqui <43079783+melquisedeque-magalhaes@users.noreply.github.com>
2021-02-24 01:33:48 -0300

    Update Profile.tsx

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -5,7 +5,7 @@ export default function Profile () {
         <div className={styles.profileContainer}>
             <img src="https://github.com/melquisedeque-magalhaes.png" alt="Melqui Magalhães"/>
             <div>
-                <strong>Melqui Sodré e Kassia Teixeira de Lima para sempre</strong>
+                <strong>Melqui Sodré</strong>
                 <p>
                     <img src="icons/level.svg" alt="Level"/>
                     Level 1
